55e8a8dd0036f5787180a89ffbe780a82de8f50f
James Mikolai Salazar <jamesmikolaisalazar@gmail.com>
2026-03-17 11:27:45 +0800

    Delete Frontend/src directory

--- Frontend/src/components/Message.tsx
+++
@@ -1,9 +0,0 @@
-function Message({ text }: { text: string }) {
-    return (
-        <h1 className="font-bold text-4xl text-gray-800 w-[100%] pb-8">
-            {text}
-        </h1>
-    )
-}
-
-export default Message
